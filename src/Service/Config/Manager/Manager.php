8198913a95891cec2e9c031030521c1a97e76409
devosc <devosc@gmail.com>
2014-09-29 20:57:30 -0500

    Manager Service\Config

---
+++ src/Service/Config/Manager/Manager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Framework\Service\Config\Manager;
+
+use Framework\Service\Config\Child\ChildInterface;
+use Framework\Service\Config\ConfigInterface;
+use Framework\Service\Config\Child\ChildTrait;
+use Framework\Service\Config\ConfigTrait;
+
+class Manager
+    implements ChildInterface, ConfigInterface, ManagerInterface
+{
+    /**
+     *
+     */
+    use ChildTrait,
+        ConfigTrait;
+
+    /**
+     * @param string $name
+     * @param array $calls
+     */
+    public function __construct($name, array $calls = [])
+    {
+        $this->config = [
+            self::CALLS  => $calls,
+            self::MERGE  => true,
+            self::NAME   => $name,
+            self::PARENT => self::MANAGER
+        ];
+    }
+}
---
+++ src/Service/Config/Manager/ManagerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Framework\Service\Config\Manager;
+
+interface ManagerInterface
+{
+    /**
+     *
+     */
+    const MANAGER = 'Manager';
+}
