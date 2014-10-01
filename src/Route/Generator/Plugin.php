97b6af187570a2e5e39c6a77afef0dbf4bfa7003
devosc <devosc@gmail.com>
2014-10-01 01:52:01 -0500

    url()

--- src/Route/Generator/Plugin.php
+++ src/Route/Generator/Plugin.php
@@ -14,12 +14,25 @@ class Plugin
     use ServiceTrait;
 
     /**
+     * @param null $name
+     * @param array $params
+     * @return string
+     */
+    public function url($name = null, array $params = [])
+    {
+        $params  = $name ? $params : $params + $this->route()->params();
+        $route   = $name ?: $this->route()->name();
+
+        return $this->generate($route, $params);
+    }
+
+    /**
      * @param null|string $name
      * @param array $params
      * @return string
      */
     public function __invoke($name = null, array $params = [])
     {
-        return $this->generate($name ?: $this->route()->name(), $params);
+        return $this->url($name, $params);
     }
 }
--- src/Route/Generator/PluginInterface.php
+++ src/Route/Generator/PluginInterface.php
@@ -4,4 +4,10 @@ namespace Framework\Route\Generator;
 
 interface PluginInterface
 {
+    /**
+     * @param null $name
+     * @param array $params
+     * @return string
+     */
+    public function url($name = null, array $params = []);
 }
