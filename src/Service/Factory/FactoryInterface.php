b9432c7be83b13e8f6cc64b48fec17639c8afe52
devosc <devosc@gmail.com>
2014-10-14 02:37:32 -0500

    ci

--- src/Service/Factory/FactoryInterface.php
+++ src/Service/Factory/FactoryInterface.php
@@ -2,44 +2,6 @@
 
 namespace Framework\Service\Factory;
 
-use Framework\Config\ConfigInterface as Config;
-
 interface FactoryInterface
 {
-    /**
-     * @param $name
-     * @return mixed
-     */
-    function assigned($name);
-
-    /**
-     * @return Config
-     */
-    function config();
-
-    /**
-     * @param $name
-     * @return mixed
-     */
-    function configured($name);
-
-    /**
-     * @param array|object|string $config
-     * @param array $args
-     * @return null|object
-     */
-    function create($config, array $args = []);
-
-    /**
-     * @param string $name
-     * @param array $args
-     * @return null|object
-     */
-    function get($name, array $args = []);
-
-    /**
-     * @param $name
-     * @return object
-     */
-    function service($name);
 }
