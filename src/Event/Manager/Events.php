7466a1cd7fb7f3db9e5fc3b0bd8404123740fd61
devosc <devosc@gmail.com>
2014-10-29 23:35:34 -0500

    ci

--- src/Event/Manager/Events.php
+++ src/Event/Manager/Events.php
@@ -34,7 +34,7 @@ trait Events
     }
 
     /**
-     * @param Event|string $event
+     * @param callable|Event|string $event
      * @param callable $listener
      * @param array $args
      * @param callable $callback
@@ -42,8 +42,6 @@ trait Events
      */
     protected function emit($event, callable $listener, array $args = [], callable $callback = null)
     {
-        /** @var callable $event */
-
         return is_callable($event) ? $event($listener, $args, $callback) : $this->invoke($listener, $args, $callback);
     }
 }
