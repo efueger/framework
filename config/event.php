b8a1c0143009ab37fcf933efccd1956febef9eb3
devosc <devosc@gmail.com>
2014-11-25 05:31:32 -0600

    ci

--- config/event.php
+++ config/event.php
@@ -4,46 +4,42 @@
  */
 
 return [
-    'Controller\Dispatch' => [
-        ['Controller\Dispatcher']
-    ],
-    'Controller\Exception' => [
-        ['Controller\Exception\Controller']
-    ],
-    'Exception\View' => [
-        ['Exception\Renderer']
-    ],
-    'Mvc' => [
-        ['Mvc\Route'],
-        ['Mvc\Controller'],
-        ['Mvc\Layout'],
-        ['Mvc\View'],
-        ['Mvc\Response']
-    ],
-    'Response\Dispatch' => [
-        ['Response\Sender']
-    ],
-    'Response\Exception' => [
-        ['Response\Exception\Dispatch'],
-        ['Response\Exception\Renderer']
-    ],
-    'Route\Match' => [
-        [
-            'Route\Match\Scheme',
-            'Route\Match\Hostname',
-            'Route\Match\Path',
-            'Route\Match\Wildcard',
-            'Route\Match\Method'
-        ]
-    ],
-    'Route\Dispatch' => [
-        [
-            'Route\Dispatch\Filter',
-            'Router',
-            'Route\Dispatch\Error'
-        ]
-    ],
-    'View\Render' => [
-        ['View\Renderer']
-    ],
+    'Controller\Dispatch' => [[
+        'Controller\Dispatcher'
+    ]],
+    'Controller\Exception' => [[
+        'Controller\Exception\Controller'
+    ]],
+    'Exception\View' => [[
+        'Exception\Renderer'
+    ]],
+    'Mvc' => [[
+        'Mvc\Route',
+        'Mvc\Controller',
+        'Mvc\Layout',
+        'Mvc\View',
+        'Mvc\Response'
+    ]],
+    'Response\Dispatch' => [[
+        'Response\Sender'
+    ]],
+    'Response\Exception' => [[
+        'Response\Exception\Dispatch',
+        'Response\Exception\Renderer'
+    ]],
+    'Route\Match' => [[
+        'Route\Match\Scheme',
+        'Route\Match\Hostname',
+        'Route\Match\Path',
+        'Route\Match\Wildcard',
+        'Route\Match\Method'
+    ]],
+    'Route\Dispatch' => [[
+        'Route\Dispatch\Filter',
+        'Router',
+        'Route\Dispatch\Error'
+    ]],
+    'View\Render' => [[
+        'View\Renderer'
+    ]],
 ];
