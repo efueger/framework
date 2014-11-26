750c7dc2e7eecc3eadd5be37504d576ba1d96477
devosc <devosc@gmail.com>
2014-11-26 00:58:14 -0600

    don't bind sm to closures

--- src/Service/Manager/ManageService.php
+++ src/Service/Manager/ManageService.php
@@ -38,7 +38,7 @@ trait ManageService
         }
 
         if ($config instanceof Closure) {
-            return $this->invoke($config->bindTo($this), $args, $callback);
+            return $this->invoke($config, $args, $callback);
         }
 
         return $this->resolve($config, $args);
