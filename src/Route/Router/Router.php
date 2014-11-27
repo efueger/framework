dfa609b607019563207dbb75dcacd8d0896bc1af
devosc <devosc@gmail.com>
2014-11-26 20:42:24 -0600

    ci

--- src/Route/Router/Router.php
+++ src/Route/Router/Router.php
@@ -53,10 +53,9 @@ class Router
         }
 
         foreach($definition->children() as $name => $definition) {
-
             $route->set(Route::NAME, ($this->name() == $route->name() ? null : $route->name() . '/') . $name);
 
-            if ($match = $this->dispatch(clone $route, $this->create($definition))) {
+            if ($match = $this->dispatch($route, $this->create($definition))) {
                 return $match;
             }
         }
