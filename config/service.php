32909fcb25ce54ffcf699f6adcd21f2e52594f69
devosc <devosc@gmail.com>
2014-11-29 04:13:27 -0600

    ci

--- config/service.php
+++ config/service.php
@@ -3,6 +3,8 @@
  *
  */
 
+namespace Framework;
+
 use Framework\Service\Config\Args\Args;
 use Framework\Service\Config\Call\Call;
 use Framework\Service\Config\ConfigLink\ConfigLink;
@@ -18,73 +20,73 @@ use Framework\Service\Config\ServiceManagerLink\ServiceManagerLink;
 
 return [
     'Config'                => new ConfigLink,
-    'Controller\Action'     => Framework\Controller\Action\Action::class,
-    'Controller\Dispatch'   => Framework\Controller\Dispatch\Dispatch::class,
+    'Controller\Action'     => Controller\Action\Action::class,
+    'Controller\Dispatch'   => Controller\Dispatch\Dispatch::class,
     'Controller\Dispatcher' => new Hydrator(
-        Framework\Controller\Dispatch\Dispatcher::class,
+        Controller\Dispatch\Dispatcher::class,
         ['setControllerManager' => new Dependency('Controller\Manager')]
     ),
     'Controller\Error' => new Hydrator(
-        Framework\Controller\Error\Controller::class,
+        Controller\Error\Controller::class,
         [
             'setModel' => new Service(
-                Framework\Controller\Error\Model::class,
+                Controller\Error\Model::class,
                 ['error/404', ['message' => 'A 404 error occurred']]
             )
         ]
     ),
-    'Controller\Exception' => Framework\Controller\Exception\Dispatch::class,
+    'Controller\Exception' => Controller\Exception\Dispatch::class,
     'Controller\Exception\Controller' => new Hydrator(
-        Framework\Controller\Exception\Controller::class,
+        Controller\Exception\Controller::class,
         ['setModel' => new Dependency('Exception\Model')]
     ),
-    'Controller\Manager' => new Manager(Framework\Controller\Manager\Manager::class),
+    'Controller\Manager' => new Manager(Controller\Manager\Manager::class),
     'Exception\Renderer' => new Hydrator(
-        Framework\View\Exception\Renderer::class,
+        View\Exception\Renderer::class,
         ['setViewManager' => new Dependency('View\Manager')]
     ),
     'Exception\View' => new Hydrator(
-        Framework\View\Exception\View::class,
+        View\Exception\View::class,
         ['setModel' => new Dependency('Exception\Model')]
     ),
-    'Exception\Model' => new Service(Framework\View\Exception\Model::class, ['error/exception']),
+    'Exception\Model' => new Service(View\Exception\Model::class, ['error/exception']),
     'Factory' => new Service(null, [new ServiceManagerLink]),
-    'Layout'  => new Service(Framework\View\Layout\Model::class, ['layout']),
+    'Layout'  => new Service(View\Layout\Model::class, ['layout']),
     'Manager' => new Hydrator(null, [
         'aliases'       => new Param('alias'),
         'configuration' => new ConfigLink,
         'events'        => new Param('events'),
         'services'      => new Param('services'),
     ]),
-    'Mvc' => new Service(Framework\Mvc\Mvc::class, [new ServiceManagerLink]),
+    'Mvc' => new Service(Mvc\Mvc::class, [new ServiceManagerLink]),
     'Mvc\Controller' => new Hydrator(
-        Framework\Mvc\Controller\Dispatcher::class,
+        Mvc\Controller\Dispatcher::class,
         ['setControllerManager' => new Dependency('Controller\Manager')]
     ),
-    'Mvc\Layout' => Framework\Mvc\Layout\Renderer::class,
+    'Mvc\Layout' => Mvc\Layout\Renderer::class,
     'Mvc\Response' => new Hydrator(
-        Framework\Mvc\Response\Dispatcher::class,
+        Mvc\Response\Dispatcher::class,
         ['setResponseManager' => new Dependency('Response\Manager')]
     ),
     'Mvc\Route' => new Hydrator(
-        Framework\Mvc\Route\Router::class,
+        Mvc\Route\Router::class,
         ['setRouteManager' => new Dependency('Route\Manager')]
     ),
     'Mvc\View' => new Hydrator(
-        Framework\Mvc\View\Renderer::class,
+        Mvc\View\Renderer::class,
         ['setViewManager' => new Dependency('View\Manager')]
     ),
-    'Response\Dispatch'  => Framework\Response\Dispatch::class,
-    'Response\Exception' => Framework\Response\Exception\Exception::class,
+    'Response\Dispatch'  => Response\Dispatch::class,
+    'Response\Exception' => Response\Exception\Exception::class,
     'Response\Exception\Dispatch' => new Service(
-        Framework\Response\Exception\Dispatcher::class,
+        Response\Exception\Dispatcher::class,
         [new Hydrator('Response', ['setStatus' => 500])]
     ),
-    'Response\Exception\Renderer' => Framework\Response\Exception\Renderer::class,
-    'Response\Sender'             => Framework\Response\Sender::class,
-    'Response\Manager'            => new Manager(Framework\Response\Manager\Manager::class),
+    'Response\Exception\Renderer' => Response\Exception\Renderer::class,
+    'Response\Sender'             => Response\Sender::class,
+    'Response\Manager'            => new Manager(Response\Manager\Manager::class),
     'Route' => new Service(
-        Framework\Route\Config::class,
+        Route\Config::class,
         [
             new Args([
                 'hostname' => new Call('request.getHost'),
@@ -95,12 +97,12 @@ return [
         ]
     ),
     'Route\Builder' => new Service(
-        Framework\Route\Definition\Builder\Builder::class,
+        Route\Definition\Builder\Builder::class,
         [new Param('routes')]
     ),
-    'Route\Dispatch'        => Framework\Route\Router\Dispatch::class,
+    'Route\Dispatch'        => Route\Router\Dispatch::class,
     'Route\Dispatch\Error'  => new Invokable(new ServiceConfig('Route\Error')),
-    'Route\Dispatch\Filter' => Framework\Route\Router\Filter::class,
+    'Route\Dispatch\Filter' => Route\Router\Filter::class,
     'Route\Error' => new Service(
         'Route',
         [
@@ -115,34 +117,34 @@ return [
         ]
     ),
     'Route\Generator' => new Service(
-        Framework\Route\Generator\Generator::class,
+        Route\Generator\Generator::class,
         [new Param('routes'), new Invoke('Route\Builder')]
     ),
-    'Route\Manager'        => new Manager(Framework\Route\Manager\Manager::class),
-    'Route\Match'          => Framework\Route\Match\Match::class,
-    'Route\Match\Hostname' => Framework\Route\Match\Hostname\Hostname::class,
-    'Route\Match\Method'   => Framework\Route\Match\Method\Method::class,
-    'Route\Match\Path'     => Framework\Route\Match\Path\Path::class,
-    'Route\Match\Scheme'   => Framework\Route\Match\Scheme\Scheme::class,
-    'Route\Match\Wildcard' => Framework\Route\Match\Wildcard\Wildcard::class,
+    'Route\Manager'        => new Manager(Route\Manager\Manager::class),
+    'Route\Match'          => Route\Match\Match::class,
+    'Route\Match\Hostname' => Route\Match\Hostname\Hostname::class,
+    'Route\Match\Method'   => Route\Match\Method\Method::class,
+    'Route\Match\Path'     => Route\Match\Path\Path::class,
+    'Route\Match\Scheme'   => Route\Match\Scheme\Scheme::class,
+    'Route\Match\Wildcard' => Route\Match\Wildcard\Wildcard::class,
     'Route\Plugin' => new Hydrator(
-        Framework\Route\Generator\GeneratorPlugin::class,
+        Route\Generator\GeneratorPlugin::class,
         [
             'setRoute'          => new Dependency('Route'),
             'setRouteGenerator' => new Dependency('Route\Generator')
         ]
     ),
     'Router' => new Service(
-        Framework\Route\Router\Router::class,
+        Route\Router\Router::class,
         [new Param('routes')],
         ['setRouteManager' => new Dependency('Route\Manager')]
     ),
     'Service\Manager' => new ServiceManagerLink,
-    'View\Manager'    => new Manager(Framework\View\Manager\Manager::class),
-    'View\Model'      => Framework\View\Model\Model::class,
-    'View\Render'     => Framework\View\Render\Render::class,
+    'View\Manager'    => new Manager(View\Manager\Manager::class),
+    'View\Model'      => View\Model\Model::class,
+    'View\Render'     => View\Render\Render::class,
     'View\Renderer'   => new Hydrator(
-        Framework\View\Renderer\Renderer::class,
+        View\Renderer\Renderer::class,
         [
             'templates'      => new Param('templates'),
             'setViewManager' => new Dependency('View\Manager')
