2c61e13a737f4c709c2dfe7978905dce63dc39fe
devosc <devosc@gmail.com>
2014-12-24 00:23:41 -0600

    ci

--- config/templates.php
+++ config/templates.php
@@ -4,8 +4,8 @@
  */
 
 return [
-    'home'            => __DIR__ . '/../view/home.phtml',
-    'layout'          => __DIR__ . '/../view/layout.phtml',
+    'error/404'       => __DIR__ . '/../view/404.phtml',
     'error/exception' => __DIR__ . '/../view/exception.phtml',
-    'error/404'       => __DIR__ . '/../view/404.phtml'
+    'home'            => __DIR__ . '/../view/home.phtml',
+    'layout'          => __DIR__ . '/../view/layout.phtml'
 ];
