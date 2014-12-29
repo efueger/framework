14fb615862c307c4d773119630281c1bc3d163b1
devosc <devosc@gmail.com>
2014-12-29 14:17:54 -0600

    ci

--- src/Route/Generator/Generator.php
+++ src/Route/Generator/Generator.php
@@ -3,17 +3,17 @@
 namespace Framework\Route\Generator;
 
 use Exception;
-use Framework\Route\Builder\Args;
+use Framework\Route\Builder\Compiler;
 use Framework\Route\Definition\Definition;
-use InvalidArgumentException;
 
-/**
- * Portions copyright (c) 2013 Ben Scholzen 'DASPRiD'. (http://github.com/DASPRiD/Dash)
- * under the Simplified BSD License (http://opensource.org/licenses/BSD-2-Clause).
- */
 class Generator
     implements RouteGenerator
 {
+    /**
+     *
+     */
+    use Compiler;
+
     /**
      * @var callable
      */
@@ -64,90 +64,6 @@ class Generator
         }
 
         return $url;
-    }
-
-    /**
-     * @param $tokens
-     * @param $args
-     * @param $defaults
-     * @return mixed
-     * @throws InvalidArgumentException
-     */
-    protected function compile($tokens, $args, $defaults)
-    {
-        $stack = [];
-
-        $current = [
-            'is_optional' => false,
-            'skip'        => true,
-            'skippable'   => false,
-            'path'        => '',
-        ];
-
-        foreach($tokens as $part) {
-            switch($part[Args::TYPE]) {
-                case 'literal':
-
-                    $current['path'] .= $part[Args::LITERAL];
-
-                    break;
-
-                case 'parameter':
-
-                    $current['skippable'] = true;
-
-                    if (!isset($args[$part[Args::NAME]])) {
-
-                        if (!$current['is_optional']) {
-                            throw new InvalidArgumentException(sprintf('Missing parameter "%s"', $part[Args::NAME]));
-                        }
-
-                        continue;
-                    }
-
-                    if (!$current['is_optional']
-                                || !isset($defaults[$part[Args::NAME]])
-                                    || $defaults[$part[Args::NAME]] !== $args[$part[Args::NAME]]
-
-                    ) {
-
-                        $current['skip'] = false;
-
-                    }
-
-                    $current['path'] .= $args[$part[Args::NAME]];
-
-                    break;
-
-                case 'optional-start':
-
-                    $stack[] = $current;
-
-                    $current = [
-                        'is_optional' => true,
-                        'skip'        => true,
-                        'skippable'   => false,
-                        'path'        => '',
-                    ];
-
-                    break;
-
-                case 'optional-end':
-
-                    $parent = array_pop($stack);
-
-                    if (!($current['path'] !== '' && $current['is_optional'] && $current['skippable'] && $current['skip'])) {
-                        $parent['path'] .= $current['path'];
-                        $parent['skip'] = false;
-                    }
-
-                    $current = $parent;
-
-                    break;
-            }
-        }
-
-        return $current['path'];
     }
 
     /**
