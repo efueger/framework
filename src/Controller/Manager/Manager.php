42740001f923c13964afe79d499701d8da5b446a
devosc <devosc@gmail.com>
2015-01-01 02:52:19 -0600

    simple

--- src/Controller/Manager/Manager.php
+++ src/Controller/Manager/Manager.php
@@ -2,7 +2,11 @@
 
 namespace Framework\Controller\Manager;
 
+use Exception;
+use Framework\Controller\Dispatch\Controller;
+use Framework\Controller\Exception\DispatchException;
 use Framework\Event\Manager\EventManager;
+use Framework\Event\Manager\Events;
 use Framework\Service\Manager\ServiceManager;
 
 class Manager
@@ -11,5 +15,44 @@ class Manager
     /**
      *
      */
-    use Manage;
+    use Events;
+
+    /**
+     * @param callable $controller
+     * @param array $args
+     * @return mixed
+     */
+    public function action(callable $controller, array $args = [])
+    {
+        return $this->call($controller, $args, $this);
+    }
+
+    /**
+     * @param array|callable|object|string $config
+     * @return callable
+     */
+    public function controller($config)
+    {
+        return $this->invokable($config);
+    }
+
+    /**
+     * @param callable $controller
+     * @param array $args
+     * @return mixed
+     */
+    public function dispatch(callable $controller, array $args = [])
+    {
+        return $this->trigger([Controller::DISPATCH, $controller], $args, $this);
+    }
+
+    /**
+     * @param Exception $exception
+     * @param array $args
+     * @return mixed
+     */
+    public function exception(Exception $exception, array $args = [])
+    {
+        return $this->trigger([DispatchException::EXCEPTION, $exception], $args, $this);
+    }
 }
--- src/Route/Manager/Manager.php
+++ src/Route/Manager/Manager.php
@@ -3,6 +3,11 @@
 namespace Framework\Route\Manager;
 
 use Framework\Event\Manager\EventManager;
+use Framework\Event\Manager\Events;
+use Framework\Route\Definition\Definition;
+use Framework\Route\Router\RouteDispatch;
+use Framework\Route\Match\RouteMatch;
+use Framework\Route\Route;
 use Framework\Service\Manager\ServiceManager;
 
 class Manager
@@ -11,5 +16,34 @@ class Manager
     /**
      *
      */
-    use Manage;
+    use Events;
+
+    /**
+     * @param array|Definition $definition
+     * @return Definition
+     */
+    public function definition($definition)
+    {
+        return $this->call(Args::CREATE, [Args::DEFINITION => $definition]);
+    }
+
+    /**
+     * @param Definition $definition
+     * @param Route $route
+     * @return Route
+     */
+    public function match(Definition $definition, Route $route)
+    {
+        return $this->trigger([RouteMatch::ROUTE, $definition, $route], [], $this);
+    }
+
+    /**
+     * @param Route $route
+     * @param array $args
+     * @return Route
+     */
+    public function route(Route $route, array $args = [])
+    {
+        return $this->trigger([RouteDispatch::DISPATCH, $route], $args, $this);
+    }
 }
